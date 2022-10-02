cca18a721a10d3f2a2cf2900cbd12c4485e0cdc2
Aitor <1726644+aaitor@users.noreply.github.com>
2022-10-02 21:23:14 +0200

    Adding search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -183,6 +183,21 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'IB2HKPUD05',
+        // Public API key: it is safe to commit it
+        apiKey: '3d8368dd627ea8c7a748d249ff26a567',
+        indexName: 'nevermined',
+        // Optional
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+      },
     }),
 };
 
